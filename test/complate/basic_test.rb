abfa9afde3df9779d519b34fe43b5213c6d16123
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2017-10-22 09:22:22 +0200

    Add very simple streaming test

--- test/complate/basic_test.rb
+++ test/complate/basic_test.rb
@@ -13,9 +13,18 @@ class Complate::BasicTest < Minitest::Test
     assert_equal 'Arguments: 1, 2, 3', capture(@renderer.render('1', '2', '3'))
   end
 
-  def test_additional_scripts
+  def test_that_its_possible_to_load_additional_scripts
     renderer = Complate::Renderer.new('test/dist/simple_bundle.js', 'test/dist/additional_scripts.js')
     assert_equal 'Add: 21', capture(renderer.render('add', 10, 11))
+  end
+
+  def test_that_streaming_basically_works
+    index = 0
+    @renderer.render('streaming').each do |s|
+      assert_equal "Block #{index}", s
+      index += 1
+    end
+    assert_equal 3, index
   end
 
 end
--- test/dist/simple_bundle.js
+++ test/dist/simple_bundle.js
@@ -2,6 +2,12 @@ function render(stream, a, b, c) {
   if (a === 'add') {
     stream.write("Add: " + add(b, c));
   }
+  else if (a === 'streaming') {
+    for (var i = 0; i<3; i++) {
+      stream.write('Block ' + i);
+      stream.flush();
+    }
+  }
   else {
     var args = Array.prototype.slice.call(arguments, 1);
     stream.write("Arguments: " + args.join(', ') );
