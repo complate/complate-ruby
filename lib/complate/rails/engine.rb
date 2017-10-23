9f03209cba1701a86dcdc404529484c80e9bafa1
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2017-10-23 09:40:02 +0200

    Add access to rails helper methods

--- lib/complate/rails/engine.rb
+++ lib/complate/rails/engine.rb
@@ -4,12 +4,27 @@ module Complate
     class Engine < ::Rails::Engine
     end
 
+    class HelpersWrapper
+
+      def initialize(controller)
+        @controller = controller
+      end
+
+      def [](name)
+        -> (*args) {
+          @controller.helpers.send(name.to_s, *args[1..-1])
+        }
+      end
+
+    end
+
     module ActionControllerExtensions
 
       extend ActiveSupport::Concern
 
       def complate(*args)
         renderer = Complate::Renderer.new('dist/bundle.js')
+        renderer.context['rails'] = self.helpers
         self.response_body = renderer.render(*args)
       end
     end
